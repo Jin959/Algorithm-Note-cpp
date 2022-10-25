55852d9804f4297edeea544bbf1e7ea51fcb25d3
Jin959 <soe959@gmail.com>
2022-10-25 17:05:29 +0900

    update 2d_array_rotation, adding clockwise rotation

--- Matrix(Array)_Handling/2d_array_rotation-general_method.cpp
+++ Matrix(Array)_Handling/2d_array_rotation-general_method.cpp
@@ -1,6 +1,15 @@
-// counter clockwise rotation of 3 by 3 array
+//  rotation of 3 by 3 array
 
-void rotate(int arr[][3])
+void copy_result(int tmp[][3], int arr[][3])
+{
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			arr[y][x] = tmp[y][x];
+		}
+	}
+}
+
+void rotate_left(int arr[][3])  //counter clockwise
 {
 	int tmp[3][3] = { 0 };
 	for (int y = 0; y < 3; y++) {
@@ -9,9 +18,19 @@ void rotate(int arr[][3])
 		}
 	}
 
-	for (int y = 0; y < 3; y++) {
-		for (int x = 0; x < 3; x++) {
-			arr[y][x] = tmp[y][x];
+	copy_result(tmp, arr);
+}
+
+void rotate_right(int arr[][3]) // clockwise
+{
+	int tmp[3][3] = { 0 };
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			tmp[x][2 - y] = arr[y][x];
 		}
 	}
+
+	copy_result(tmp, arr);
 }
